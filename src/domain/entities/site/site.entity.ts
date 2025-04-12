6ae2e2a2c23ce691c737d47caee1389978683025
alizzycraft <fix.society.github@gmail.com>
2025-04-13 00:29:33 +0200

    Add site entity

---
+++ src/domain/entities/site/site.entity.ts
@@ -0,0 +1,31 @@
+export class Site {
+    constructor(
+      public id: string,
+      public name: string,
+      public description?: string,
+      public pageOrder: string[] = []
+    ) {}
+  
+    static fromJSON(json: {
+      id: string;
+      name: string;
+      description?: string;
+      pageOrder?: string[];
+    }): Site {
+      return new Site(
+        json.id,
+        json.name,
+        json.description,
+        json.pageOrder ?? []
+      );
+    }
+  
+    toJSON(): Record<string, unknown> {
+      return {
+        id: this.id,
+        name: this.name,
+        description: this.description,
+        pageOrder: this.pageOrder,
+      };
+    }
+  }
